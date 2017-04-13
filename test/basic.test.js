23f072afa0517eeeb6e6907f64af17e9830b44c1
Alex Wilson <alex.wilson@joyent.com>
2017-04-13 13:55:15 -0700

    joyent/node-artedi#1 want library for collecting metrics from manta services

---
+++ test/basic.test.js
@@ -0,0 +1,15 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Copyright (c) 2017, Joyent, Inc.
+ */
+
+var mod_tape = require('tape');
+
+var mod_artedi = require('..');
+
+mod_tape.test('empty test', function (t) {
+	t.end();
+});
